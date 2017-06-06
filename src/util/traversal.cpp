833cc0182d6c5963dffc237ea20842c751070db0
Denis Blank <denis.blank@outlook.com>
2017-06-06 22:44:06 +0200

    more

--- src/util/traversal.cpp
+++ src/util/traversal.cpp
@@ -7,14 +7,17 @@
 
 #include <hpx/config.hpp>
 #include <hpx/util/tuple.hpp>
+#include <hpx/traits/is_range.hpp>
+
+#include <hpx/util/invoke_fused.hpp>
 
 // #include <hpx/traits/is_future.hpp>
 
 // #include <hpx/lcos/future.hpp>
 // #include <hpx/traits/future_traits.hpp>
 //
-// #include <hpx/traits/is_future_range.hpp>
-// #include <hpx/traits/is_future_tuple.hpp>
+//
+//
 // #include <hpx/util/detail/pack.hpp>
 // #include <hpx/util/invoke_fused.hpp>
 // #include <hpx/util/lazy_enable_if.hpp>
@@ -51,10 +54,16 @@ template <typename... T> void unused(T&&... args) {
 }
 */
 
-template <typename Mapper, typename T>
-auto map_single_element(Mapper&& map, T&& element)
+/// Tag for dispatching based on the sequenceable or container requirements
+template <bool IsContainer, bool IsSequenceable>
+struct container_match_tag
 {
-}
+};
+
+template <typename T>
+using container_match_of = container_match_tag<hpx::traits::is_range<T>::value,
+
+    >;
 
 /// A helper class which applies the mapping or routes the element through
 template <typename M>
@@ -74,23 +83,56 @@ public:
         return element;
     }*/
 
+    /// Traverses a single element
     template <typename T>
-    auto traverse(T&& element) const
+    auto traverse(T&& element) const -> T
     {
+        // TODO Check statically, whether we should traverse the element
         return element;
     }
 
+    /// Calls the traversal method for every element in the pack
     template <typename... T>
-    auto traverse_pack(T&&... pack)
+    auto traverse_pack(/*Remapper remapper,*/ T&&... pack)
         -> decltype(hpx::util::make_tuple(traverse(std::forward<T>(pack))...))
     {
-        // TODO check statically whether we should traverse the whole pack
+        // TODO Check statically, whether we should traverse the whole pack
+        //
+        // TODO Use a remapper instead of fixed hpx::util::make_tuple
         return hpx::util::make_tuple(traverse(std::forward<T>(pack))...);
+    }
+
+private:
+    /// Match plain elments
+    template <typename T>
+    auto match(container_match_tag<false, false>, T&& element)
+        -> decltype(mapper_(std::forward<T>(element)))
+    {
+        return mapper_(std::forward<T>(element));
+    }
+
+    /// Match elements satisfying the container requirements,
+    /// which are not sequenced.
+    template <typename T>
+    auto match(container_match_tag<true, false>, T&& element)
+    {
+        // TODO
+    }
+
+    /// Match elements which are sequenced and that are also may
+    /// satisfying the container requirements.
+    template <bool IsContainer, typename T>
+    auto match(container_match_tag<IsContainer, true>, T&& element)
+    {
+        // TODO
     }
 };
 
+/// Traverses the given pack with the given mapper
 template <typename Mapper, typename... T>
-auto traverse_pack(Mapper&& mapper, T&&... pack)
+auto traverse_pack(Mapper&& mapper, T&&... pack) -> decltype(
+    std::declval<mapping_helper<typename std::decay<Mapper>::type>>()
+        .traverse_pack(std::forward<T>(pack)...))
 {
     mapping_helper<typename std::decay<Mapper>::type> helper(
         std::forward<Mapper>(mapper));
