7fd3c8b0ffb57d5de1a53698b499e0496c7a830b
Denis Blank <denis.blank@outlook.com>
2017-06-25 00:40:13 +0200

    The feature test...

---
+++ cmake/tests/cxx11_sfinae_expression_complete.cpp
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2017 Denis Blank
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#include <hpx/util/pack_traversal.hpp>
+
+int main()
+{
+}
