01dc3ae9eea81517b63f332d89fe7261fb0775ec
Denis Blank <denis.blank@outlook.com>
2017-07-11 18:45:53 +0200

    more

--- mylightunwraptest.cpp
+++ mylightunwraptest.cpp
@@ -4,24 +4,43 @@
 using namespace hpx::util;
 using namespace hpx::util::detail;
 
-template <typename C, typename... T>
-void apply_flatten_impl(std::false_type, C&& callable, T&&... args)
+/// A callable object which returns its input wrapped inside a tuple
+struct tupelize
 {
-    // TODO Check whether any of the args is a detail::flatted_tuple_t,
-    //      if not, use the linear called version for better compilation speed.
+    template <typename... T>
+    tuple<T...> operator()(T&&... args) const
+    {
+        return tuple<T...>{std::forward<T>(args)...};
+    }
+};
+
+/// Use the linear instantiation for non flat types
+template <typename C, typename... T>
+auto apply_flatten_impl(std::false_type, C&& callable, T&&... args)
+{
+    // TODO bla
+    return 0;
 }
 
-template<typename... T>
-using is_any_flatten_t = any_of<
+/// Use the recursive instantiation for a variadic pack which may contain
+/// flat types
+template <typename C, typename... T>
+void apply_flatten_impl(std::true_type, C&& callable, T&&... args)
+{
+}
 
->;
+template <typename... T>
+using is_any_flatten_t = any_of<is_flat<T>...>;
 
 template <typename C, typename... T>
 auto apply_flatten(C&& callable, T&&... args)
+    -> decltype(apply_flatten_impl(is_any_flatten_t<T...>{},
+        std::forward<C>(callable), std::forward<T>(args)...))
 {
     // Check whether any of the args is a detail::flatted_tuple_t,
     // if not, use the linear called version for better compilation speed.
-    return apply_flatten_impl(std::forward<C>(callable), std::forward<T>(args)...);
+    return apply_flatten_impl(is_any_flatten_t<T...>{},
+        std::forward<C>(callable), std::forward<T>(args)...);
 }
 
 int main(int, char**)
