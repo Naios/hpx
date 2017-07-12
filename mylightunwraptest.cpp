172236f164991289c011948f43cdb1eb8135c2cf
Denis Blank <denis.blank@outlook.com>
2017-07-12 12:41:40 +0200

    more

--- mylightunwraptest.cpp
+++ mylightunwraptest.cpp
@@ -5,8 +5,43 @@ namespace hpx {
 namespace util {
     namespace detail {
         namespace spreading {
+            /// Use the linear instantiation for variadic packs which don't
+            /// contain spread types.
+            template <typename C, typename... T>
+            auto apply_spread_impl(std::false_type, C&& callable, T&&... args)
+                -> typename invoke_result<C, T...>::type
+            {
+                return invoke(
+                    std::forward<C>(callable), std::forward<T>(args)...);
+            }
+
+            /// Use the recursive instantiation for a variadic pack which
+            /// may contain spread types
+            template <typename C, typename... T>
+            void apply_spread_impl(std::true_type, C&& callable, T&&... args)
+            {
+            }
+
+            /// Deduces to a true_type if any of the given types marks
+            /// the underlying type to be spread into the current context.
+            template <typename... T>
+            using is_any_spread_t = any_of<is_spread<T>...>;
+
+            template <typename C, typename... T>
+            auto apply_spread(C&& callable, T&&... args)
+            /*-> decltype(apply_spread_impl(is_any_spread_t<T...>{},
+                    std::forward<C>(callable), std::forward<T>(args)...))*/
+            {
+                // Check whether any of the args is a detail::flatted_tuple_t,
+                // if not, use the linear called version for better
+                // compilation speed.
+                // TODO Maybe short circuit this
+                return apply_spread_impl(is_any_spread_t<T...>{},
+                    std::forward<C>(callable), std::forward<T>(args)...);
+            }
+
             /// A callable object which returns its input wrapped inside a tuple
-            struct tupelize
+            struct functional_tupelize
             {
                 template <typename... T>
                 tuple<T...> operator()(T&&... args) const
@@ -15,34 +50,12 @@ namespace util {
                 }
             };
 
-            /// Use the linear instantiation for non flat types
-            template <typename C, typename... T>
-            auto apply_flatten_impl(std::false_type, C&& callable, T&&... args)
-            {
-                // TODO bla
-                return 0;
-            }
-
-            /// Use the recursive instantiation for a variadic pack which
-            /// may contain flat types
-            template <typename C, typename... T>
-            void apply_flatten_impl(std::true_type, C&& callable, T&&... args)
-            {
-            }
-
             template <typename... T>
-            using is_any_spread_t = any_of<is_spread<T>...>;
-
-            template <typename C, typename... T>
-            auto apply_flatten(C&& callable, T&&... args)
-                -> decltype(apply_flatten_impl(is_any_spread_t<T...>{},
-                    std::forward<C>(callable), std::forward<T>(args)...))
+            auto tupelize(T&&... args) /*-> decltype(
+                apply_spread(functional_tupelize{}, std::forward<T>(args)...))*/
             {
-                // Check whether any of the args is a detail::flatted_tuple_t,
-                // if not, use the linear called version for better
-                // compilation speed.
-                return apply_flatten_impl(is_any_spread_t<T...>{},
-                    std::forward<C>(callable), std::forward<T>(args)...);
+                return apply_spread(
+                    functional_tupelize{}, std::forward<T>(args)...);
             }
         }
     }
@@ -51,8 +64,11 @@ namespace util {
 
 int main(int, char**)
 {
-    using namespace hpx::util;
-    using namespace hpx::util::detail;
+    using hpx::util::map_pack;
+    using hpx::util::spread_this;
+    using namespace hpx::util::detail::spreading;
+
+    tupelize(0, 0, spread_this(0, 0));
 
     map_pack([](int) -> float { return 0.f; }, 0, 0);
 
