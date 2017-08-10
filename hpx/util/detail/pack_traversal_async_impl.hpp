88d72dffb8364b801c2bce5e5a973ad455314336
Denis Blank <denis.blank@outlook.com>
2017-08-10 21:37:02 +0200

    more

--- hpx/util/detail/pack_traversal_async_impl.hpp
+++ hpx/util/detail/pack_traversal_async_impl.hpp
@@ -8,12 +8,14 @@
 
 #include <hpx/config.hpp>
 #include <hpx/util/always_void.hpp>
+#include <hpx/util/detail/pack.hpp>
 #include <hpx/util/detail/pack_traversal_impl.hpp>
 #include <hpx/util/invoke.hpp>
+#include <hpx/util/tuple.hpp>
 
+#include <exception>
 #include <functional>    // reference_wrapper
 #include <memory>
-#include <tuple>
 #include <type_traits>
 #include <utility>
 
@@ -50,39 +52,104 @@ namespace util {
             }
         };
 
+        struct async_traversal_detached_exception : std::exception
+        {
+            explicit async_traversal_detached_exception()
+            {
+            }
+
+            ~async_traversal_detached_exception() throw()
+            {
+            }
+
+            const char* what() const throw()
+            {
+                return "";
+            }
+        };
+
         template <typename From, typename To>
         struct async_range
         {
         };
 
-        template <typename Target, std::size_t... Sequence>
+        template <typename Target, std::size_t Begin, std::size_t End>
         struct static_async_range
         {
             Target* target_;
 
-            HPX_CONSTEXPR Target* operator*() const noexcept
+            /*HPX_CONSTEXPR auto operator*() const noexcept
+                -> decltype(util::get<Current>(target_))
             {
-                return target_;
-            }
+                return util::get<Current>(target_);
+            }*/
+
+            /*HPX_CONSTEXPR static_async_range<Target, Rest...> shift() const
+                noexcept
+            {
+                return static_async_range<Target, Rest...>{target_};
+            }*/
         };
 
-        /// Trav
-        template <typename Frame, typename Target, std::size_t... Sequence,
-            typename... Rest>
-        bool async_traverse(Frame& frame,
-            static_async_range<Target, Sequence...> range, Rest&... rest)
+        template <typename Frame, typename Current, typename Capture>
+        void async_traverse_one(
+            Frame&& frame, Current&& element, Capture&& capture)
         {
-            bool aborted = false;
+            if (frame->traverse(*element))
+            {
+                // The Traversal should continue synchronously
+                return;
+            }
 
-            return aborted;
+            // If the traversal method returns false, we detach the
+            // current execution context and call the visitor with the
+            // element and a continue callable object again.
+
+            // TODO
+
+            proceed = false;
         }
 
-        /// Reenter an asynchronous iterator pack
+        /*template<std::size_t I, typename Frame, typename Range, typename Rest>
+        void invoke_static_async_range(Frame& frame, Range& range, Rest &rest)
+        {
+            async_traverse_one(range.template<I> adjust()
+        }*/
+
+        template <typename Frame, std::size_t... Sequence, typename Range,
+            typename Rest>
+        bool async_traverse_static_async_range(Frame& frame,
+            pack_c<std::size_t, Sequence...>, Range& range, Rest& rest)
+        {
+            bool proceed = false;
+
+            auto invoker = [&](auto&& element) {
+                if (proceed)
+                {
+                    proceed = async_traverse_one(element, rest);
+                }
+            };
+
+            int dummy[] = {0, ((void) (range.template at<Sequence>()), 0)...};
+            (void) dummy;
+
+            return proceed;
+        }
+
+        template <typename Frame, typename Target, std::size_t Begin,
+            std::size_t End, typename Rest>
+        bool async_traverse(Frame& frame,
+            static_async_range<Target, Begin, End> range, Rest& rest)
+        {
+        }
+
+        /// Reenter an asynchronous iterator pack and continue its traversal.
         template <typename Current, typename Next, typename... Rest>
         bool reenter(Current& current, Next& next, Rest&... rest)
         {
             if (async_traverse(std::forward<Current>(current),
-                    std::forward<Next>(next), std::forward<Rest>(current)...))
+                    util::forward_as_tuple(std::forward<Next>(next),
+                        std::forward<Rest>(current)...)))
             {
                 return reenter(std::forward<Next>(next).shift(),
                     std::forward<Rest>(current)...);
@@ -100,6 +167,8 @@ namespace util {
 
             auto frame = std::make_shared<FrameType>(
                 std::forward<Mapper>(mapper), std::forward<T>(pack));
+
+            frame->head();
         }
     }    // end namespace detail
 }    // end namespace util
